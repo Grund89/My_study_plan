1a5442668a3917797f161bde2caf76e525996bb0
Douglas Grund <douglasgrund@yahoo.com.br>
2024-07-09 15:05:24 -0300

    feat: adiciona código para somar dois números inteiros e exibir o resultado

---
+++ operacoes.rb
@@ -0,0 +1,7 @@
+puts 'Soma de dois números'
+print 'Digite o primeiro número inteiro: '
+number1 = gets.chomp.to_i
+print 'Digite o segundo número: '
+number2 = gets.chomp.to_i
+addition = number1 + number2
+puts "O resultado da soma entre os dois números é: #{addition}"
