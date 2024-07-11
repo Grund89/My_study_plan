b433c74d90f7a9322ccc0206019c4fd6728ec417
Douglas Grund <douglasgrund@yahoo.com.br>
2024-07-11 14:56:02 -0300

    feat: adiciona código para exibir resultados de operações aritméticas

--- operacoes.rb
+++ operacoes.rb
@@ -2,3 +2,9 @@ print 'Digite um número inteiro: '
 n1 = gets.chomp.to_i
 print 'Digite o segundo número: '
 n2 = gets.chomp.to_i
+
+
+puts "A soma é : #{soma} "
+puts "A subtração é : #{subtracao} "
+puts "A multiplicação é : #{multiplicacao} "
+puts "A divisão é : #{divisao} "
