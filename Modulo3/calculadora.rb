5e06f5e60f70638d303e06890c4e17f55d7c69ea
Douglas Grund <douglasgrund@yahoo.com.br>
2024-07-18 15:51:32 -0300

    build: first commit

---
+++ Modulo3/calculadora.rb
@@ -0,0 +1,10 @@
+result = ''
+loop do
+  puts 'Selecione a opção:'
+  puts '1 - Multiplicar 2 números'
+  puts '2 - Dividir 2 números'
+  puts '3 - Adicionar 2 números'
+  puts '4 - Subtrair 2 números'
+  puts '0 - Sair'
+
+  option = gets.chomp.to_i
