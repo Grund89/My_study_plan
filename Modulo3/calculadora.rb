4a80c8c51a847eee50ed40353cd41356a795a0da
Douglas Grund <douglasgrund@yahoo.com.br>
2024-07-18 15:58:24 -0300

    feat: adiciona cálculo de multiplicação de dois números fornecidos pelo usuário

--- Modulo3/calculadora.rb
+++ Modulo3/calculadora.rb
@@ -8,3 +8,14 @@ loop do
   puts '0 - Sair'
 
   option = gets.chomp.to_i
+
+  if option == 1
+    print 'Digite o primeiro número: '
+    n1 = gets.chomp.to_i
+
+    print 'Digite o segundo número: '
+    n2 = gets.chomp.to_i
+
+    multiplication = n1 * n2
+
+    result = "A multiplicação de #{n1} x #{n2} é #{multiplication}"
