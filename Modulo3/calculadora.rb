98d9df67627a4395d207cb03ae0922a37a6db1eb
Douglas Grund <douglasgrund@yahoo.com.br>
2024-07-18 16:10:12 -0300

    feat: adiciona operações aritméticas e tratamento de opções inválidas

--- Modulo3/calculadora.rb
+++ Modulo3/calculadora.rb
@@ -19,3 +19,45 @@ loop do
     multiplication = n1 * n2
 
     result = "A multiplicação de #{n1} x #{n2} é #{multiplication}"
+
+  elsif option == 2
+    print 'Digite o primeiro número: '
+    n1 = gets.chomp.to_i
+
+    print 'Digite o segundo número: '
+    n2 = gets.chomp.to_i
+
+    division = n1 / n2
+
+    result = "A divisão de #{n1} / #{n2} é #{division}"
+
+  elsif option == 3
+    print 'Digite o primeiro número: '
+    n1 = gets.chomp.to_i
+
+    print 'Digite o segundo número: '
+    n2 = gets.chomp.to_i
+
+    addition = n1 + n2
+
+    result = "A soma de #{n1} + #{n2} é #{addition}"
+
+  elsif option == 4
+    print 'Digite o primeiro número: '
+    n1 = gets.chomp.to_i
+
+    print 'Digite o segundo número: '
+    n2 = gets.chomp.to_i
+
+    subtraction = n1 - n2
+
+    result = "A subtração de #{n1} - #{n2} é #{subtraction}"
+
+  elsif option == 0
+    break
+
+  else
+    result = 'Opção inválida, digite umas das opções listadas, ou 0 para SAIR.'
+  end
+  system "clear"
+end
