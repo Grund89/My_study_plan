491a70242e727fbcde322f3854ed7b0ae84124e8
Douglas Grund <douglasgrund@yahoo.com.br>
2024-07-18 15:34:03 -0300

    feat: adiciona cálculo de idade com base no ano de nascimento e ano atual

--- Modulo3/age.rb
+++ Modulo3/age.rb
@@ -7,3 +7,11 @@ loop do
   print 'Opção : '
 
   option = gets.chomp.to_i
+  if option == 1
+    print 'Digite o ano de nascimento: '
+    year_for_birth = gets.chomp.to_i
+
+    print 'Digite o ano atual: '
+    current_year = gets.chomp.to_i
+    age = current_year - year_for_birth
+    result = "Quem nasceu no ano de #{year_for_birth}, tem #{age} anos em #{current_year} "
