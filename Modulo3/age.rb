e0bf4b5a89777415b540c4b06e88442f0e89ddae
Douglas Grund <douglasgrund@yahoo.com.br>
2024-07-18 15:37:03 -0300

    feat: adiciona tratamento para opções inválidas e finalização do loop

--- Modulo3/age.rb
+++ Modulo3/age.rb
@@ -15,3 +15,12 @@ loop do
     current_year = gets.chomp.to_i
     age = current_year - year_for_birth
     result = "Quem nasceu no ano de #{year_for_birth}, tem #{age} anos em #{current_year} "
+
+  elsif option == 0
+    break
+
+  else
+    result = 'Opção inválida'
+  end
+   system "clear"
+end
