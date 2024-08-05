8ec3acd774f1e68e9627081eb7046937f62ad4d4
Douglas Grund <douglasgrund@yahoo.com.br>
2024-08-05 14:55:23 -0300

    feat: adiciona operações aritméticas com base na escolha do usuário e tratamento de opções inválidas

--- Modulo3/calculadora_v2.rb
+++ Modulo3/calculadora_v2.rb
@@ -7,3 +7,32 @@ loop do
   puts '3- Multiplicar'
   puts '4- Dividir'
   puts '0- Sair'
+
+  option = gets.chomp.to_i
+
+  case option
+  when 1..4
+    print 'Digite o primeiro número: '
+    n1 = gets.chomp.to_i
+
+    print  'Digite o segundo número: '
+    n2 = gets.chomp.to_i
+
+    case option
+    when 1
+      result = "#{n1} + #{n2} = #{n1 + n2}"
+    when 2
+      result = "#{n1} - #{n2} = #{n1 - n2}"
+    when 3
+      result = "#{n1} x #{n2} = #{n1 * n2}"
+    when 4
+      result = "#{n1} / #{n2} = #{n1 / n2}"
+    end
+  when 0
+    break
+  else
+    reseult = 'Opção inválida'
+  end
+  # comando que limpa o console
+    system "clear"
+end
