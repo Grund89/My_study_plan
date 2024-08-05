0dc47206596e34593c94b11af60c396cd80ea56f
Douglas Grund <douglasgrund@yahoo.com.br>
2024-08-05 14:46:52 -0300

    build: first commit

---
+++ Modulo3/calculadora_v2.rb
@@ -0,0 +1,9 @@
+result = ""
+loop do
+  puts result
+  puts 'Selecione uma das seguintes opções: '
+  puts '1- Adicionar'
+  puts '2- Subtrair'
+  puts '3- Multiplicar'
+  puts '4- Dividir'
+  puts '0- Sair'
