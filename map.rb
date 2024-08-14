8d4416a950eafe800fe8cd6ec7f1b73eb523f475
Douglas Grund <douglasgrund@yahoo.com.br>
2024-08-14 15:14:42 -0300

    refactor: renomeia variável no bloco map para maior clareza

--- map.rb
+++ map.rb
@@ -2,8 +2,8 @@ array = [1, 2, 3, 4]
 # /n é uma quebra de linha
 puts "\n Executando .map multiplicando cada item por 2"
 # .map não altera o conteúdo do array original
-new_array = array.map do |a|
-  a * 2
+new_array = array.map do |element|
+  element * 2
 end
 
 puts "\n Array Original"
