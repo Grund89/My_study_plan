d93964c5e06b869fc79258a7e924ba3d234445a4
Douglas Grund <douglasgrund@yahoo.com.br>
2024-08-22 15:29:04 -0300

    feat: adiciona código para coletar três números e armazená-los em um array

--- Modulo4/missao_01.rb
+++ Modulo4/missao_01.rb
@@ -1,3 +1,13 @@
 # Utilizando uma collection do tipo Array, escreva um programa que
 # receba 3 números e no final exiba o resultado de cada um deles
 # elevado a segunda potência.
+
+array = []
+
+i = 1
+
+3.times do
+  print "Digite o #{i}º número: "
+  array.push gets.chomp.to_i
+  i += 1
+end
