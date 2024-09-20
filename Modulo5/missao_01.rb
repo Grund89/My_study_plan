be30fd2aaae2d27307ec0c4048f2b7536651e608
Douglas Grund <douglasgrund@yahoo.com.br>
2024-09-20 01:33:53 -0300

    feat: adicionar método para calcular a potência de um número com base e expoente definidos pelo usuário

--- Modulo5/missao_01.rb
+++ Modulo5/missao_01.rb
@@ -1,2 +1,18 @@
 # Crie um programa que possua um metodo que resolva a potencia dado um numero base
 # e seu expoente. Estes dois valores devem ser informados pelo usuario
+
+# Método para calcular a potência
+def calcular_potencia(base, expoente)
+  base ** expoente
+end
+
+# Solicitando valores do usuário
+print "Informe o número base: "
+base = gets.chomp.to_i
+
+print "Informe o expoente: "
+expoente = gets.chomp.to_i
+
+# Chamando o método e exibindo o resultado
+resultado = calcular_potencia(base, expoente)
+puts "O resultado de #{base} elevado a #{expoente} é: #{resultado}"
