0362db1e6de0609970f0f4f911d20a495211da32
Douglas Grund <douglasgrund@yahoo.com.br>
2024-09-23 10:41:54 -0300

    feat: define um metodo para validar um cpf

--- Modulo5/missao02/missao_02.rb
+++ Modulo5/missao02/missao_02.rb
@@ -1,2 +1,12 @@
-# Siga a documentação da gem cpf_cnpj para creiar um programa que recebe como
+# Siga a documentação da gem cpf_cnpj para criar um programa que recebe como
 # entrada um numero de cpf e em um método verifique se esse número é válido.
+
+require "cpf_cnpj"
+
+def check_cpf(cpf_number)
+  if CPF.valid?(cpf_number)
+    puts "Este é um CPF válido!"
+  else
+    puts "Este não é um CPF válido!"
+  end
+end
