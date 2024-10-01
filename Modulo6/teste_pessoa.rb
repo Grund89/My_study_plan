891652cb5092a9de391ec5441d8f062486d65bb6
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-01 20:44:18 -0300

    test: adicionar testes unitários para verificar nome e cálculo de idade na classe Pessoa

--- Modulo6/teste_pessoa.rb
+++ Modulo6/teste_pessoa.rb
@@ -1,2 +1,17 @@
 require 'minitest/autorun'
 require_relative 'pessoa'  # Certifique-se de que o nome do arquivo está correto
+
+class TestPessoa < Minitest::Test
+  def setup
+    @pessoa = Pessoa.new("João", 1990)  # Inicializa uma instância da classe Pessoa
+  end
+
+  def test_nome
+    assert_equal "João", @pessoa.name
+  end
+
+  def test_calculo_idade
+    idade_esperada = Time.now.year - 1990
+    assert_equal idade_esperada, @pessoa.calcular_idade
+  end
+end
