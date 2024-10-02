920ff39d5a2fe4c1553abe2bb406f7d8015f9199
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-02 15:58:11 -0300

    fix: corrige logica

--- Modulo7/variavel_global.rb
+++ Modulo7/variavel_global.rb
@@ -1,11 +1,19 @@
-class Baz
-  def qux
-   $global += 1
-   puts $global
+class Bar
+  def foo
+    $global = 0  # Define uma variável global com o valor 0
+    puts $global  # Imprime o valor da variável global
   end
 end
- bar = Bar.new
- baz = Baz.new
- bar.foo
- baz.qux
- puts $global
+
+class Baz
+  def qux
+    $global += 1  # Incrementa o valor da variável global em 1
+    puts $global  # Imprime o valor atualizado da variável global
+  end
+end
+
+bar = Bar.new  # Cria uma instância da classe Bar
+baz = Baz.new  # Cria uma instância da classe Baz
+bar.foo        # Chama o método foo da classe Bar, que define e imprime $global
+baz.qux        # Chama o método qux da classe Baz, que incrementa e imprime $global
+baz.qux        # Chama o método qux da classe Baz, que incrementa e imprime $global
