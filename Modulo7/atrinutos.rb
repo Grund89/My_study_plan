a38d343b3581199f00205aa66be37c21f1bf9abb
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-04 21:09:47 -0300

    feat: adicionar atributos  e  com  na classe Dog

--- Modulo7/atrinutos.rb
+++ Modulo7/atrinutos.rb
@@ -1,15 +1,28 @@
-class Dog
-  def name
-    @name
-  end
+# class Dog
+#   def name
+#     @name
+#   end
 
-  def name= name
-    @name = name
-  end
+#   def name= name
+#     @name = name
+#   end
+# end
+
+# dog = Dog.new
+# Í
+# dog.name = 'Marlon'
+
+# puts dog.name
+
+class Dog
+  attr_accessor :name, :age
 end
 
-dog = Dog.new
-Í
-dog.name = 'Marlon'
 
+dog = Dog.new
+
+dog.name = 'Marlon'
 puts dog.name
+
+dog.age = '1 ano'
+puts dog.age
