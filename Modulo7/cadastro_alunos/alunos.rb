76dd8810bcb70467f93a0db1c6ca46e20313de9a
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-07 15:11:11 -0300

    feat: adiciona metodo de inicializador e exibição de informações

--- Modulo7/cadastro_alunos/alunos.rb
+++ Modulo7/cadastro_alunos/alunos.rb
@@ -1,2 +1,12 @@
 class Aluno
   attr_accessor :nome, :idade, :curso, :genero
+
+  def initialize (nome, idade, curso, genero)
+    @nome = nome
+    @idade = idade
+    @curso = curso
+    @genero = genero
+  end
+
+  def exibir_informacoes
+    puts "Nome: #{@nome}, Idade: #{@idade}, Curso: #{@curso}, Gênero: #{@genero}"
