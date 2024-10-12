64e0056d0260285a03c9ede82bc080314ccc26d4
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-12 01:44:26 -0300

    feat: executa bloco com yield

--- Modulo8/blocks.rb
+++ Modulo8/blocks.rb
@@ -5,11 +5,18 @@
 # numbers.each { |number| sum += number }
 # puts sum
 
-foo = {2 => 3, 4 => 5}
+# foo = {2 => 3, 4 => 5}
 
-foo.each do |key, value|
-  puts "key = #{key}"
-  puts "value = #{value}"
-  puts " key * value = #{key * value}"
-  puts '----'
+# foo.each do |key, value|
+#   puts "key = #{key}"
+#   puts "value = #{value}"
+#   puts "key * value = #{key * value}"
+#   puts '----'
+# end
+
+def foo
+  yield
+  yield
 end
+
+foo = { puts 'Exec the block'}
