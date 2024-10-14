bc66864aeaf905b8ab13948f54981f8406fec15e
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-14 20:47:56 -0300

    feat: verifica existência de bloco

--- Modulo8/blocks.rb
+++ Modulo8/blocks.rb
@@ -14,18 +14,27 @@
 #   puts '----'
 # end
 
-def meu_iterador
-  yield 1
-  yield 2
-  yield 3
-end
+# def meu_iterador
+#   yield 1
+#   yield 2
+#   yield 3
+# end
 
-meu_iterador do |valor|
-  puts "Valor: #{valor}"
-end
+# meu_iterador do |valor|
+#   puts "Valor: #{valor}"
+# end
 
 # Explicando:
 
 # A função meu_iterador define um bloco de código que será executado quando a função for chamada.
 # Cada yield pausa a execução da função e passa o valor especificado para o bloco.
 # O bloco de código recebe o valor através do parâmetro valor e o imprime.
+
+
+def foo
+  if block_given?
+    yield
+  else
+    puts "Sem parâmetro do tipo bloco"
+  end
+end
