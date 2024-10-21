d56bda1406277feb2b3e0929a74907d9edb54a64
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-21 20:51:56 -0300

    feat: adiciona parametros dentro de uma lambda

--- Modulo8/lambda.rb
+++ Modulo8/lambda.rb
@@ -1,5 +1,11 @@
 # first_lambda = lambda { puts 'my first lambda' }
 # first_lambda.call
 
-first_lambda = -> { puts 'my first lambda' }
-first_lambda.call
+# first_lambda = -> { puts 'my first lambda' }
+# first_lambda.call
+
+first_lambda = -> (names){ names.each { |name |puts name} }
+
+names = {"João", "Maria", "Pedro"}
+
+first_lambda.call(names)
