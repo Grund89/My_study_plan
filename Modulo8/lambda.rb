9c6054e73939d26e430f71af85198e927599d63a
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-21 21:28:49 -0300

    feat: adiciona função com duas lambdas

--- Modulo8/lambda.rb
+++ Modulo8/lambda.rb
@@ -10,17 +10,27 @@
 
 # first_lambda.call(names)
 
-my_lambda = lambda do |numbers|
-  index = 0
-  puts 'Número atual + Próximo número'
+# my_lambda = lambda do |numbers|
+#   index = 0
+#   puts 'Número atual + Próximo número'
 
-  while index < numbers.length - 1
-    puts "(#{numbers[index]}) + (#{numbers[index + 1]})"
-    puts numbers[index] + numbers[index + 1]
-    index += 1
-  end
+#   while index < numbers.length - 1
+#     puts "(#{numbers[index]}) + (#{numbers[index + 1]})"
+#     puts numbers[index] + numbers[index + 1]
+#     index += 1
+#   end
+# end
+
+# numbers = [1, 2, 3, 4]
+
+# my_lambda.call(numbers)
+
+def foo(first_lambda, second_lambda)
+  first_lambda.call
+  second_lambda.call
 end
 
-numbers = [1, 2, 3, 4]
+first_lambda = -> { puts 'my first lambda' }
+second_lambda = -> { puts 'my second lambda'}
 
-my_lambda.call(numbers)
+foo(first_lambda, second_lambda)
