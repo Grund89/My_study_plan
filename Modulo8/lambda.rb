010aeb5431e9e4ceaa74c7a36ae0fab5ce4cafab
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-21 21:22:25 -0300

    feat: cria lambda com loop para somar numeros

--- Modulo8/lambda.rb
+++ Modulo8/lambda.rb
@@ -4,8 +4,23 @@
 # first_lambda = -> { puts 'my first lambda' }
 # first_lambda.call
 
-first_lambda = -> (names){ names.each { |name |puts name} }
+# first_lambda = -> (names){ names.each { |name |puts name} }
 
-names = ["João", "Maria", "Pedro"]
+# names = ["João", "Maria", "Pedro"]
 
-first_lambda.call(names)
+# first_lambda.call(names)
+
+my_lambda = lambda do |numbers|
+  index = 0
+  puts 'Número atual + Próximo número'
+
+  while index < numbers.length - 1
+    puts "(#{numbers[index]}) + (#{numbers[index + 1]})"
+    puts numbers[index] + numbers[index + 1]
+    index += 1
+  end
+end
+
+numbers = [1, 2, 3, 4]
+
+my_lambda.call(numbers)
